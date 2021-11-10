65390687a054d5fc623176bebf8674b623bb43f6
Fredrik Brännbacka <fredrik.brannbacka@ilpvfx.com>
2021-11-10 10:48:59 +0100

    Update build requires

--- package.py
+++ package.py
@@ -11,11 +11,13 @@ variants = [
 def build_requires():
     # check if the system gcc is too old <9
     # then we require devtoolset-9
+    requirements = ["cmake-3.15+<4"]
     from subprocess import check_output
-    valid = check_output(r"expr `gcc -dumpversion | cut -f1 -d.` \>= 9 || true", shell=True).strip().decode() == "1"
-    if not valid:
-        return ["devtoolset-9"]
-    return []
+    gcc_major = int(check_output(r"gcc -dumpversion | cut -f1 -d.", shell=True).strip().decode())
+    if gcc_major < 9:
+        requirements.append("devtoolset-9")
+
+    return requirements
 
 build_command = "make -f {root}/Makefile {install}"
 
