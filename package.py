42e682d6b323e09702eaad6a3c45a02185ebcc41
Fredrik Brännbacka <fredrik.brannbacka@ilpvfx.com>
2021-11-10 09:56:41 +0100

    add devtoolset as build_requires

--- package.py
+++ package.py
@@ -7,6 +7,16 @@ variants = [
     ["platform-linux", "python-3.7"]
 ]
 
+@early()
+def build_requires():
+    # check if the system gcc is too old <9
+    # then we require devtoolset-9
+    from subprocess import check_output
+    valid = check_output(r"expr `gcc -dumpversion | cut -f1 -d.` \>= 9 || true", shell=True).strip().decode() == "1"
+    if not valid:
+        return ["devtoolset-9"]
+    return []
+
 build_command = "make -f {root}/Makefile {install}"
 
 def commands():
