018111df11b67b39bebbcb7f2e807b20f917a36b
Fredrik Brännbacka <fredrik.brannbacka@ilpvfx.com>
2026-08-10 11:54:10 +0200

    chore: move to gcc package as a private_build_requires

--- package.py
+++ package.py
@@ -8,17 +8,13 @@ variants = [
     ["platform-linux", "python-3.11"],
 ]
 
-@early()
-def build_requires():
-    # check if the system gcc is too old <9
-    # then we require devtoolset-9
-    requirements = ["cmake-3.15+<4"]
-    from subprocess import check_output
-    gcc_major = int(check_output(r"gcc -dumpversion | cut -f1 -d.", shell=True).strip().decode())
-    if gcc_major < 9:
-        requirements.append("devtoolset-9")
+private_build_requires = [
+    "gcc-11"
+]
 
-    return requirements
+build_requires = [
+    "cmake-3.15+<4"
+]
 
 build_command = "make -f {root}/Makefile {install}"
 
